9d4e906cc13313aa357f9930c7f45b084b85c990
maritocuate <mario300@gmail.com>
2024-08-12 20:56:05 -0300

    ✨ home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ export default function Home() {
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">Derecho IA</p>
+        <p className="text-sm font-semibold text-gray-700">
+          Asesoramiento Penal Instantáneo con IA
+        </p>
       </div>
       <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-        Chateá con el código penal con{' '}
-        <span className="text-blue-600">Inteligencia Artificial</span>
+        Chateá con <span className="text-blue-600">Abotgado</span>, el mejor
+        asesor legal.
       </h1>
       <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-        PDF Buddy allows you to have conversations with any PDF document. Simply
-        upload your file and start asking questions right away.
+        Obtén respuestas precisas y rápidas sobre derecho penal en cualquier
+        momento.
       </p>
     </MaxWidthWrapper>
   )
