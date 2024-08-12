f9386ea0cfa9a09dbfe40b76ce7beb216ab1dff9
maritocuate <mario300@gmail.com>
2024-08-12 20:14:53 -0300

    ✨ home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Derecho IA
-    </main>
-  );
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">Derecho IA</p>
+      </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chateá con el código penal con{' '}
+        <span className="text-blue-600">Inteligencia Artificial</span>
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        PDF Buddy allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away.
+      </p>
+    </MaxWidthWrapper>
+  )
 }
