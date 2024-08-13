791e4b9c75f48348c634f6401816694eceb64965
maritocuate <mario300@gmail.com>
2024-08-13 18:26:11 -0300

    ✨ home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import { Fira_Sans as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Fira_Sans as FontSans } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import './globals.css'
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 export const metadata: Metadata = {
-  title: "Derecho IA",
+  title: 'Derecho IA',
   description:
-    "Inteligencia artificial al servicio de la constitución argentina.",
-};
+    'Inteligencia artificial al servicio de la constitución argentina.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="grainy">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased grainy",
+          'min-h-screen bg-background font-sans antialiased grainy',
           fontSans.variable
         )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
+import './styles.css'
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex align-top justify-between">
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 sm:flex-row sm:text-left text-center flex-col-reverse flex align-top justify-between">
       <div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chateá con <span className="text-blue-600">Legisbot</span>,<br />
           tu asesor legal.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Asesoramiento Penal Instantáneo con IA.
+          Asesoramiento penal instantáneo con IA.
         </p>
       </div>
-      <div>
+      <div className="sm:block mb-5 flex justify-center">
         <Image
           src="/images/legisbot.png"
           alt="Legisbot"
           sizes="100%"
           width={0}
           height={0}
-          style={{ width: '100px', height: 'auto' }}
+          className="legisbot"
         />
       </div>
     </MaxWidthWrapper>
