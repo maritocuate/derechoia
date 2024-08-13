403ee3b61038cb0df581cdaa0fb3ac6d4cea85f9
maritocuate <mario300@gmail.com>
2024-08-13 19:17:21 -0300

    ✨ kinde login add

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,16 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
 import './styles.css'
-import { Button } from '@/components/ui/button'
+import { buttonVariants } from '@/components/ui/button'
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const { getUser, isAuthenticated } = getKindeServerSession()
+  const user = await getUser()
+  const authStatus = await isAuthenticated()
+
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 sm:flex-row sm:text-left text-center flex-col-reverse flex align-top justify-between">
       <div>
@@ -14,7 +21,27 @@ export default function Home() {
         <p className="mt-2 max-w-prose text-zinc-700 sm:text-lg">
           Asesoramiento penal instantáneo con IA.
         </p>
-        <Button className="p-5 mt-4 bg-blue-600">Empezá</Button>
+        {/* <Button className="p-5 mt-4 bg-blue-600">Empezá</Button> */}
+        {!user && !authStatus ? (
+          <LoginLink
+            className={buttonVariants({
+              size: 'lg',
+              className: 'mt-5',
+            })}
+          >
+            Ingresá
+          </LoginLink>
+        ) : (
+          <Link
+            className={buttonVariants({
+              size: 'lg',
+              className: 'mt-5',
+            })}
+            href="/dashboard"
+          >
+            Empezá
+          </Link>
+        )}
       </div>
       <div className="sm:block mb-5 flex justify-center">
         <Image
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
+
 export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <LogoutLink>Logout</LogoutLink>
     </div>
   )
 }
