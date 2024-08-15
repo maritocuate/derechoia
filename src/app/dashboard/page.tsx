98bb03fedccf445c9d8564e30ae22876a1edc19d
maritocuate <mario300@gmail.com>
2024-08-15 14:34:29 -0300

    ✨ dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
+import { Navbar } from '../components/Navbar'
 
 export default async function Dashboard() {
   const { isAuthenticated } = getKindeServerSession()
@@ -10,6 +11,7 @@ export default async function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Navbar />
     </div>
   )
 }
