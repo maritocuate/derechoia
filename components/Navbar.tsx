91527973b0f427da9a13610c874cea175093e2a4
maritocuate <mario300@gmail.com>
2024-08-16 20:19:19 -0300

    🐛 debug prod

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,15 @@
 import { LogoutButton } from './LogoutButton'
-import { SubscriptionButton } from './SubscriptionButton'
-import { checkSubscription } from '@/lib/subscription'
+/* import { SubscriptionButton } from './SubscriptionButton'
+import { checkSubscription } from '@/lib/subscription' */
 
 export const Navbar = async () => {
-  const isPro = await checkSubscription()
+  // const isPro = await checkSubscription()
 
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <LogoutButton />
-        <SubscriptionButton isPro={isPro} />
+        {/* <SubscriptionButton isPro={isPro} /> */}
       </div>
     </div>
   )
