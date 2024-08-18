8cff0f30baaa75ffc322af4a24640abacffb4a68
maritocuate <mario300@gmail.com>
2024-08-18 16:01:54 -0300

    🐛 debug prod

--- src/lib/subscription.ts
+++ src/lib/subscription.ts
@@ -1,15 +1,13 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-/* import prismadb from '@/lib/prismadb'
+import prismadb from '@/lib/prismadb'
 
-const DAY_IN_MS = 86_400_000 */
+// const DAY_IN_MS = 86_400_000
 export const checkSubscription = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
   const userId = user?.id
 
-  if (!userId) return false
-  return true
-  /* if (!userId) {
+  if (!userId) {
     return false
   }
 
@@ -23,7 +21,9 @@ export const checkSubscription = async () => {
     },
   })
 
-  if (!userSubscription) {
+  return true
+
+  /* if (!userSubscription) {
     return false
   }
 
